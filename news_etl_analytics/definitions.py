d91fdd476acc93bed6755738acac9b4a3c067026
Omkar Pawar <omkar@Omkars-Mac-mini.local>
2025-08-14 18:22:25 +0530

    created .env file and aaded .env environment in it

--- news_etl_analytics/definitions.py
+++ news_etl_analytics/definitions.py
@@ -8,9 +8,11 @@ etl_job = define_asset_job("etl_job", selection="*")
 
 etl_schedule = ScheduleDefinition(
     job=etl_job,
-    cron_schedule="# 30 10-22 * * *  ",
-    execution_timezone="Asia/Kolkata",    
+    cron_schedule="30 10-22 * * *",  # Valid cron expression
+    execution_timezone="Asia/Kolkata"
 )
+
+
 defs = Definitions(
     assets=[assets.bronze, assets.silver, assets.gold],
     schedules=[etl_schedule],
